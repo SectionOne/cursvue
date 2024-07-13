64c92dbf174c56635b7d70124a885b30d3ead6d3
Section1 <otinoco@digiocio.com>
2024-07-13 20:50:12 +0200

    Codi 12 - Controlant events OnClick

--- 01-bases-vue/app.js
+++ 01-bases-vue/app.js
@@ -7,13 +7,17 @@ const app = createApp({
     //`,	
     setup(){
         const message = ref("Hola Món"); //L'element queda per defecte protegit
+        const presentacio = ref("");
 
+        const presentarse = () => {
+            presentacio.value = "El meu nom és Oriol";
+        }
         setTimeout(() => {
             message.value = "Hola Vue";
             console.log("Executat");
         }, 2000);
 
-        return { message };
+        return { message, presentacio, presentarse };
     }
 });
 
