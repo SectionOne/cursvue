c85bd06f655ba729781518f1d2406054166a36d4
Section1 <otinoco@digiocio.com>
2024-07-13 18:32:38 +0200

    Codi 4 - Realitzant un hola món

--- 01-bases-vue/app.js
+++ 01-bases-vue/app.js
@@ -1,1 +1,10 @@
-console.log(vue);
+const { createApp,ref } = Vue;
+
+const app = createApp({
+    template: `	
+        <h1>Hola Món</h1>
+        <p>Desde app.js</p>
+    `	
+})
+
+app.mount('#myApp');
