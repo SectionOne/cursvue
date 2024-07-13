8ac2e4c9982945dba4f0e72e3912f61eeacd6257
Section1 <otinoco@digiocio.com>
2024-07-13 21:41:34 +0200

    Codi 15 - Desetructurant objectes

--- 02-vfor/app.js
+++ 02-vfor/app.js
@@ -3,11 +3,11 @@ const {createApp, ref} = Vue;
 const app = createApp({
     setup(){
         const players = ref([
-            {id: 1, name: 'Rafael'},
-            {id: 2, name: 'Messi'},
-            {id: 3, name: 'Luis'},
-            {id: 4, name: 'Cristiano'},
-            {id: 5, name: 'Neymar'}
+            {id: 1, name: 'Rafael',number: 10},
+            {id: 2, name: 'Messi',number: 9},
+            {id: 3, name: 'Luis',number: 13},
+            {id: 4, name: 'Cristiano',number: 7},
+            {id: 5, name: 'Neymar',number: 4}
         ]);
         return {players};
     }    
