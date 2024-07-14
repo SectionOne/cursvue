ec2e7e163feb3d4f858bc5f7f7a235775583ba86
Section1 <otinoco@digiocio.com>
2024-07-14 16:55:19 +0200

    34 - Definint funcionalitat

--- 03-vue-project/src/App.vue
+++ 03-vue-project/src/App.vue
@@ -1,10 +1,19 @@
 <template>
-    <h1>Hola món</h1>
+    <section>
+        <h3>Contador: {{ counter }}</h3>
+        <h3>Quadrat: {{ 10 * 10 }}</h3>
+
+        <div>
+            <button>+1</button>
+            <button>-1</button>
+        </div>
+    </section>
 </template>
 
 <!--Definim la configuració de Vue amb un setup-->
 <script lang="ts" setup>
-console.log("Executat");
+import {ref} from 'vue';
+const counter = ref(10);
 </script>
 
 <!--Emprem scoped perque només s'apliqui a aquest component i template-->
