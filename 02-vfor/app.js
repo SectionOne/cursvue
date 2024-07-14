b6b08ced914513eaef8f22f7df104d7f3f354dcb
Section1 <otinoco@digiocio.com>
2024-07-14 11:59:41 +0200

    Codi 17 - Com funciona el V-if

--- 02-vfor/app.js
+++ 02-vfor/app.js
@@ -2,6 +2,9 @@ const {createApp, ref} = Vue;
 
 const app = createApp({
     setup(){
+
+        const showPlayer = ref(true);
+
         const players = ref([
             {id: 1, name: 'Rafael',number: 10},
             {id: 2, name: 'Messi',number: 9},
@@ -9,7 +12,7 @@ const app = createApp({
             {id: 4, name: 'Cristiano',number: 7},
             {id: 5, name: 'Neymar',number: 4}
         ]);
-        return {players};
+        return {players, showPlayer};
     }    
 });
 app.mount('#myApp');
