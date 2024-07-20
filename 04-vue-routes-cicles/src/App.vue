011f2a2f0500437f65ad505938183acf4bc41670
Section1 <otinoco@digiocio.com>
2024-07-20 18:21:21 +0200

    Codi 50 - Treballant amb detalls i contingut

--- 04-vue-routes-cicles/src/App.vue
+++ 04-vue-routes-cicles/src/App.vue
@@ -29,7 +29,7 @@ import { RouterView } from 'vue-router';
     <!-- Footer -->
     <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
       <p class="flex-1 text-sm text-gray-500 text-center">
-        © 20xx Acme Corporation. Derechos reservados
+        © {{ new Date().getFullYear() }} Cibernarium Corporation. Derechos reservados
       </p>
     </footer>
     <!-- Fin Footer -->
