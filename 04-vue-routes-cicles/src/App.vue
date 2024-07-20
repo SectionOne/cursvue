bea0af4a1e94669938fb32678ed58ec68a085e06
Section1 <otinoco@digiocio.com>
2024-07-20 16:39:38 +0200

    Codi 48 - Rutes sense hashtag

--- 04-vue-routes-cicles/src/App.vue
+++ 04-vue-routes-cicles/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <nav>
-    <a href="#/">Principal</a>
-    <a href="#/caracteristiques">Caracteristiques</a>
-    <a href="#/preus">Preus</a>
-    <a href="#/contacte">Contacte</a>
+    <RouterLink to="/">Principal</RouterLink>
+    <RouterLink to="/caracteristiques">Caracteristiques</RouterLink>
+    <RouterLink to="/preus">Preus</RouterLink>
+    <RouterLink to="/contacte">Contacte</RouterLink>
   </nav>
   <main class="bg-slate-100" >
     <RouterView />
   </main>
 </template>
 <script lang="ts" setup>
+import { RouterLink } from 'vue-router';
+
 
 </script>
--- 04-vue-routes-cicles/src/router/index.ts
+++ 04-vue-routes-cicles/src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import PaginaPrincipal from '../moduls/landing/pagines/PrincipalPage.vue';
 const router = createRouter({
   //Coneixer quines son les pagines anterior i tenir un historial de navegacions
-  history: createWebHashHistory(import.meta.env.BASE_URL), //Recuperem la variable d'entorn BASE_URL a on es troba la base de la nostre aplicació
+  history: createWebHistory(import.meta.env.BASE_URL), //Recuperem la variable d'entorn BASE_URL a on es troba la base de la nostre aplicació
   routes: [
     //Definim les diferents routes accesibles
     {
