8224203c08589384320902fe935a899682f137a6
Section1 <otinoco@digiocio.com>
2024-07-21 18:24:24 +0200

    Codi 59 - Gestionant els paràmetres

--- 04-vue-routes-cicles/src/moduls/cursos/pagines/CursosPage.vue
+++ 04-vue-routes-cicles/src/moduls/cursos/pagines/CursosPage.vue
@@ -2,13 +2,18 @@
     <section class="flex flex-col">
         <h1>Curs <small class="text-blue-500"> Introducció a Go</small></h1>
 
-    <img 
-    :src="`https://store.barcelonactiva.cat/zhofsgdjxfzvjra/img/${id}.png`" 
-    alt="Golang" 
-    class="w-[200px] h-[200px] mt-5"
-    width="200"
-    height="200"
-    />
+        <img 
+        :src="`https://store.barcelonactiva.cat/zhofsgdjxfzvjra/img/${id}.png`" 
+        alt="Golang" 
+        class="w-[200px] h-[200px] mt-5"
+        width="200"
+        height="200"
+        />
+        <RouterLink
+            class="bg-blue-500 text-white p-2 rounded mt-5 text-center"
+            :to="{name: 'Cursos', params: {id: 'javascript'}}">
+            Javascript
+        </RouterLink>
     </section>
 </template>
 
